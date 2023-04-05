46d12474259e954f3539adcf3ca6a1dd3b0aa699
braym3 <126671044+braym3@users.noreply.github.com>
2023-04-05 10:40:27 +0100

    Initial commit

---
+++ src/main/java/com/qa/ims/IMS.java
@@ -0,0 +1,96 @@
+package com.qa.ims;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.controller.Action;
+import com.qa.ims.controller.CrudController;
+import com.qa.ims.controller.CustomerController;
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.domain.Domain;
+import com.qa.ims.utils.DBUtils;
+import com.qa.ims.utils.Utils;
+
+public class IMS {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private final CustomerController customers;
+	private final Utils utils;
+
+	public IMS() {
+		this.utils = new Utils();
+		final CustomerDAO custDAO = new CustomerDAO();
+		this.customers = new CustomerController(custDAO, utils);
+	}
+
+	public void imsSystem() {
+		LOGGER.info("Welcome to the Inventory Management System!");
+		DBUtils.connect();
+
+		Domain domain = null;
+		do {
+			LOGGER.info("Which entity would you like to use?");
+			Domain.printDomains();
+
+			domain = Domain.getDomain(utils);
+
+			domainAction(domain);
+
+		} while (domain != Domain.STOP);
+	}
+
+	private void domainAction(Domain domain) {
+		boolean changeDomain = false;
+		do {
+
+			CrudController<?> active = null;
+			switch (domain) {
+			case CUSTOMER:
+				active = this.customers;
+				break;
+			case ITEM:
+				break;
+			case ORDER:
+				break;
+			case STOP:
+				return;
+			default:
+				break;
+			}
+
+			LOGGER.info(() ->"What would you like to do with " + domain.name().toLowerCase() + ":");
+
+			Action.printActions();
+			Action action = Action.getAction(utils);
+
+			if (action == Action.RETURN) {
+				changeDomain = true;
+			} else {
+				doAction(active, action);
+			}
+		} while (!changeDomain);
+	}
+
+	public void doAction(CrudController<?> crudController, Action action) {
+		switch (action) {
+		case CREATE:
+			crudController.create();
+			break;
+		case READ:
+			crudController.readAll();
+			break;
+		case UPDATE:
+			crudController.update();
+			break;
+		case DELETE:
+			crudController.delete();
+			break;
+		case RETURN:
+			break;
+		default:
+			break;
+		}
+	}
+
+}
