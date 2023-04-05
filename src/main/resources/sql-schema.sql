7098c543cbe843683d0b0ff0834abb8fc8833fc9
braym3 <60962768+mbray3@users.noreply.github.com>
2023-04-05 14:31:13 +0100

    Added drivers and warehouses to schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
+    `postcode` VARCHAR(7) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -28,7 +29,6 @@ CREATE TABLE IF NOT EXISTS `items` (
 
 CREATE TABLE IF NOT EXISTS `orders` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `order_name` VARCHAR(40) DEFAULT NULL,
     `customer_id` INT(11) NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`) ON DELETE CASCADE
@@ -43,3 +43,19 @@ CREATE TABLE IF NOT EXISTS `itemorders`
     foreign key(`item_id`) references `items`(`id`) ON DELETE CASCADE,
 	foreign key(`order_id`) references `orders`(`id`) ON DELETE CASCADE
     );
+    
+    CREATE TABLE IF NOT EXISTS `drivers` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) DEFAULT NULL,
+    `surname` VARCHAR(40) DEFAULT NULL,
+    `warehouse_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY(`warehouse_id`) REFERENCES `warehouse`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `warehouses` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    `postcode` VARCHAR(7) NOT NULL,
+    PRIMARY KEY (`id`)
+);
