cbb8cf741b79075e4f8d7ff2d5c1b1e7deea1aa5
braym3 <60962768+mbray3@users.noreply.github.com>
2023-04-06 12:05:15 +0100

    Added message for no assigned deliveries

--- src/main/java/com/qa/ims/controller/DriverController.java
+++ src/main/java/com/qa/ims/controller/DriverController.java
@@ -71,6 +71,9 @@ public class DriverController implements CrudController<Driver>{
 			for(Order order : driver.getDeliveries()) {
 				LOGGER.info(order);
 			}
+			if(driver.getDeliveries().isEmpty()) {
+				LOGGER.info("You have no assigned deliveries");
+			}
 			return driver.getDeliveries();
 		} catch (Exception e) {
 			LOGGER.info("No driver found with ID: " + id + ". Please try again");
