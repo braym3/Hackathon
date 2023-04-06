78f31f108c208356be425873c057ed7d3979c259
braym3 <60962768+mbray3@users.noreply.github.com>
2023-04-06 10:45:24 +0100

    Fixed warehouses table typo

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,7 +1,10 @@
-INSERT INTO `ims`.`warehouse` (`name`, `postcode`) VALUES ("warehouse1", "LA1 3JT");
+INSERT INTO `ims`.`warehouses` (`name`, `postcode`) VALUES ("warehouse1", "LA1 3JT");
 
 INSERT INTO `ims`.`drivers` (`first_name`, `surname`, `warehouse_id`) VALUES ("bob", "smith", 1), ("jane", "birch", 1), ("peter", "piper", 1);
 
 INSERT INTO `ims`.`customers` (`first_name`, `surname`, `postcode`) VALUES ("sarah", "brown", "LA1 6YZ");
 
-INSERT INTO `ims`.`orders` (`customer_id`, `driver_id`, `delivered`, `warehouse_id`) VALUES (1, 3, 1, 1);
+INSERT INTO `ims`.`orders` (`customer_id`, `driver_id`, `delivered`, `warehouse_id`) VALUES (1, 1, 1, 1), (1, 1, 0, 1);
+
+INSERT INTO `ims`.`items`(`item_name`, `item_value`) VALUES ("mug", 4.99), ("notebook", 6.50), ("chair", 24.99);
+
